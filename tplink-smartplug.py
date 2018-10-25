dab61ed786fa83e2ff2978a0be5f4b0f0f353af3
mikelsr <mikelsr@pm.me>
2018-10-25 16:12:50 +0200

    Port tplink-smartplug.py from python2 to python3

--- tplink-smartplug.py
+++ tplink-smartplug.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python3
 #
 # TP-Link Wi-Fi Smart Plug Protocol Client
 # For use with TP-Link HS-100 or HS-110
@@ -19,8 +19,10 @@
 # limitations under the License.
 #
 #
-import socket
 import argparse
+import socket
+from json import dumps, loads
+from struct import pack
 
 version = 0.1
 
@@ -35,68 +37,111 @@ def validIP(ip):
 
 
 # Predefined Smart Plug Commands
-# For a full list of commands, consult tplink_commands.txt
-command_list = {'info': '{"system":{"get_sysinfo":{}}}',
-                'on': '{"system":{"set_relay_state":{"state":1}}}',
-                'off': '{"system":{"set_relay_state":{"state":0}}}',
-                'cloudinfo': '{"cnCloud":{"get_info":{}}}',
-                'wlanscan': '{"netif":{"get_scaninfo":{"refresh":0}}}',
-                'time': '{"time":{"get_time":{}}}',
-                'schedule': '{"schedule":{"get_rules":{}}}',
-                'countdown': '{"count_down":{"get_rules":{}}}',
-                'antitheft': '{"anti_theft":{"get_rules":{}}}',
-                'reboot': '{"system":{"reboot":{"delay":1}}}',
-                'reset': '{"system":{"reset":{"delay":1}}}'
+# For a full list of command_list, consult tplink_commands.txt
+command_list = {"info": '{"system": {"get_sysinfo": {}}}',
+                "on": '{"system": {"set_relay_state": {"state": 1}}}',
+                "off": '{"system": {"set_relay_state": {"state": 0}}}',
+                "cloudinfo": '{"cnCloud": {"get_info": {}}}',
+                "wlanscan": '{"netif": {"get_scaninfo": {"refresh": 0}}}',
+                "time": '{"time": {"get_time": {}}}',
+                "schedule": '{"schedule": {"get_rules": {}}}',
+                "countdown": '{"count_down": {"get_rules": {}}}',
+                "antitheft": '{"anti_theft": {"get_rules": {}}}',
+                "reboot": '{"system": {"reboot": {"delay": 1}}}',
+                "reset": '{"system": {"reset": {"delay": 1}}}'
                 }
 
 
 # Encryption and Decryption of TP-Link Smart Home Protocol
 # XOR Autokey Cipher with starting key = 171
-def encrypt(string):
+def encrypt_prev_1_0_10(string):
     key = 171
-    result = "\0\0\0\0"
+    result = b'\0\0\0\0'
     for i in string:
-        a = key ^ ord(i)
+        a = key ^ i
         key = a
-        result += chr(a)
+        result += bytes([a])
     return result
+
+
+def encrypt_post_1_0_10(string):
+    key = 171
+    result = b'\0\0\0'+bytes(len(string))
+    for i in string:
+        a = key ^ i
+        key = a
+        result += bytes([a])
+    return result
+
+
+def encrypt_official(string):
+    key = 171
+    result = pack('>I', len(string))
+    for i in string:
+        a = key ^ i
+        key = a
+        result += bytes([a])
+    return result
+
+
+methods = {
+    "1": encrypt_official,
+    "2": encrypt_post_1_0_10,
+    "3": encrypt_prev_1_0_10
+}
 
 
 def decrypt(string):
     key = 171
-    result = ""
+    result = b''
     for i in string:
-        a = key ^ ord(i)
-        key = ord(i)
-        result += chr(a)
+        a = key ^ i
+        key = i
+        result += bytes([a])
     return result
 
 
 # Parse commandline arguments
 parser = argparse.ArgumentParser(description="TP-Link Wi-Fi Smart Plug Client v" + str(version))
 parser.add_argument("-t", "--target", metavar="<ip>", required=True, help="Target IP Address", type=validIP)
+parser.add_argument("-m", "--method", metavar="<method>", required=False, help="encryption method to be used", choices=["1", "2", "3"])
 group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-c", "--command", metavar="<command>", help="Preset command to send. Choices are: "+", ".join(commands), choices=commands)
+group.add_argument("-c", "--command", metavar="<command>", help="Preset command to send. Choices are: "+", ".join(command_list), choices=command_list)
 group.add_argument("-j", "--json", metavar="<JSON string>", help="Full JSON string of command to send")
 args = parser.parse_args()
 
 # Set target IP, port and command to send
 ip = args.target
+method = args.method
+
 port = 9999
 if args.command is None:
     cmd = args.json
 else:
-    cmd = commands[args.command]
+    cmd = command_list[args.command]
 
-# Send command and receive reply
-try:
-    sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock_tcp.connect((ip, port))
-    sock_tcp.send(encrypt(cmd))
-    data = sock_tcp.recv(2048)
-    sock_tcp.close()
+if args.method is not None:
+    methods = {method: methods[method]}
 
-    print "Sent:     ", cmd
-    print "Received: ", decrypt(data[4:])
-except socket.error:
-    quit("Cound not connect to host " + ip + ":" + str(port))
+if type(cmd) is str:
+    cmd = loads(cmd)
+
+err = None
+for encrypt in methods.values():
+    # Send command and receive reply
+    try:
+        sock_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock_tcp.connect((ip, port))
+        sock_tcp.send(encrypt(dumps(cmd).encode('utf-8')))
+        data = sock_tcp.recv(2048)
+        sock_tcp.close()
+        if len(data) == 0:
+            err = "Received no data"
+            continue
+        print("Sent:     ", cmd)
+        print("Received: ", decrypt(data[4:]).decode("utf-8"))
+        exit(0)
+    except socket.error as serr:
+        err = serr
+
+print("Error: %s" % err)
